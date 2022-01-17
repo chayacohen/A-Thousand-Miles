ec893e1debd46350916f12910514f574163dd17f
Brian <brianlin1590@gmail.com>
2022-01-17 11:52:01 -0500

    create session routes and validations

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
-
+const User =require("./models/User");
+const bodyParser = require("body-parser");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+    // const user = new User({
+    //     username: "test",
+    //     email: "test@test.com",
+    //     password: "password",
+    //     address: "123 st 123 ave NY"
+    // });
+    // user.save();
     res.send("Initial creation");
 });
 
---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = User = mongoose.model('users', UserSchema);
