14f6128f359898bb2d7e0c1bd856f9bba3b393ac
Nick <nbarrameda89@gmail.com>
2022-01-18 00:58:21 -0500

    itineraries reducer and root reducer update

---
+++ frontend/src/reducers/itineraries_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_ITINERARIES, RECEIVE_USER_ITINERARIES, RECEIVE_NEW_ITINERARY } from '../actions/itinerary_actions';
+  
+  const ItinerariesReducer = (state = { all: {}, user: {}, new: undefined }, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch(action.type) {
+      case RECEIVE_ITINERARIES:
+        newState.all = action.itineraries.data;
+        return newState;
+      case RECEIVE_USER_ITINERARIES:
+        newState.user = action.itineraries.data;
+        return newState;
+      case RECEIVE_NEW_ITINERARY:
+        newState.new = action.itinerary.data
+        return newState;
+      default:
+        return state;
+    }
+  };
+  
+  export default ItinerariesReducer;
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
+import itineraries from './itineraries_reducer'
 const RootReducer = combineReducers({
     session,
-    errors
+    errors,
+    itineraries
 });
 
 export default RootReducer;
