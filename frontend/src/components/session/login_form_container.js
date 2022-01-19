1dda78920f87935951922279c3d5eed4b6b39298
Nick <nbarrameda89@gmail.com>
2022-01-19 15:18:47 -0500

    signup fixed. current user available upon signup

--- frontend/src/components/session/login_form_container.js
+++ frontend/src/components/session/login_form_container.js
@@ -1,20 +1,19 @@
 import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
+import { inactiveModal } from '../../actions/modal_actions';
 import LoginForm from './login_form';
 
-const mapStateToProps = (state) => {
+const mSTP = (state) => {
     return {
         errors: state.errors.session
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mDTP = (dispatch) => {
     return {
-        login: user => dispatch(login(user))
+        login: user => dispatch(login(user)),
+        inactiveModal: () => dispatch(inactiveModal())
     }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(LoginForm);
+export default connect(mSTP, mDTP)(LoginForm);
