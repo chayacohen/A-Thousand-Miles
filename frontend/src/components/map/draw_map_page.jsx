f367099db65eb25c40bf4e6fb35a23f500801c9f
chayacohen <chayac100@gmail.com>
2022-01-19 16:20:54 -0500

    add google link by place id

--- frontend/src/components/map/draw_map_page.jsx
+++ frontend/src/components/map/draw_map_page.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Map from './map';
 import MarkerManager from "./marker_manager";
-// import googleMapAPI from "../../keys";
-
+const KEYS = require('../../keys')
 
 class DrawMapRoute extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { itineraryResults: [], totalResults: [], mapName: 'draw-map-container' }
+        this.state = { itineraryResults: [], 
+            totalResults: [], 
+            mapName: 'draw-map-container', 
+            title: '', 
+            lat: '',
+            lng: '',
+            photoUrl: '', 
+            attractionAddress: '', 
+            rating: '',
+            placeId: '', 
+            googleLink: ''
+        }
         this.clicked = false;
         this.round = true;
         this.start_pos = new google.maps.LatLng(this.props.startAddress.lat, this.props.startAddress.lng)
@@ -45,14 +55,28 @@ class DrawMapRoute extends React.Component {
                 this.map.poly.getPath().insertAt((this.path.length - 1), this.end_pos); 
                 this.path = this.map.poly.getPath().xd 
                 this.receiveResults().then(() => {
+                    debugger 
                     let increment = Math.floor(this.state.totalResults.length / 15)
                     if (increment === 0) {
                         increment = 1
                     }
                     for (let i = 0; i < this.state.totalResults.length; i += increment) {
                         const result = this.state.totalResults[i];
-                        this.state.itineraryResults.push(result); 
+                        this.state.itineraryResults.push(result);  
+                        this.service.getDetails({placeId: result.place_id}, (response, status) => {
+                            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                                this.setState({attractionAddress: response.formatted_address}) 
+                            }
+                        })
                         this.markerManager.addMarker({ lat: result.geometry.location.lat(), lng: result.geometry.location.lng() }, { url: result.icon, scaledSize: new google.maps.Size(20, 20)})
+                        this.setState({ 
+                            title: result.name, 
+                            location: [result.geometry.location.lng(), result.geometry.location.lat()], 
+                            photoUrl: result.photos[0] ? result.photos[0].getUrl() : null , 
+                            rating: result.rating, 
+                            placeId: result.place_id, 
+                            googleLink: `https://www.google.com/maps/place/?q=place_id:${result.place_id}`
+                        })
                     }
                 })
                 this.setState({mapName: 'after-draw-map-container'})
@@ -76,12 +100,12 @@ class DrawMapRoute extends React.Component {
 
     receiveResults() {
         const promises = [];
-        const service = new google.maps.places.PlacesService(this.map.map);
+        this.service = new google.maps.places.PlacesService(this.map.map);
         const increment = Math.floor(this.path.length / 15)
         for (let i = 1; i < this.path.length; i += (increment > 0 ? increment : 1)) {
             if (this.path[i])
                 promises.push(new Promise((resolve, reject) => {
-                    service.nearbySearch({
+                    this.service.nearbySearch({
                         location: { lat: this.path[i].lat(), lng: this.path[i].lng() },
                         radius: 50000,
                         type: ['tourist_attraction'],
