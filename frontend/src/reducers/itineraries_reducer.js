d9b45587b2debc40b58f09fd2522a733e9ad8098
Brian <brianlin1590@gmail.com>
2022-01-18 22:39:26 -0500

    format state so key: itinerary_id, value: itinerary

--- frontend/src/reducers/itineraries_reducer.js
+++ frontend/src/reducers/itineraries_reducer.js
@@ -4,19 +4,28 @@ import {
   REMOVE_ITINERARY,
   RECEIVE_ITINERARY
   } from '../actions/itinerary_actions';
+
+  const addKeyToObject = (state, action) => {
+    let nextState = Object.assign({}, state);
+    action.itineraries.data.forEach(itinerary => {
+      nextState[itinerary._id] = itinerary;
+    })
+    return nextState;
+  }
   
   const ItinerariesReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
     switch(action.type) {
       case RECEIVE_ITINERARIES:
-        return Object.assign({}, state, action.itineraries.data);
+        // return Object.assign({}, state, action.itineraries.data);
+        return addKeyToObject(state, action);
       case RECEIVE_USER_ITINERARIES:
-        return Object.assign({}, state, action.itineraries.data);;
+        return addKeyToObject(state, action);
       case RECEIVE_ITINERARY:
         return Object.assign({}, state, { [action.itinerary.data._id]: action.itinerary.data });
       case REMOVE_ITINERARY:
-        delete nextState[action.itinerary.id];
+        delete nextState[action.itinerary.data._id];
         return nextState;
       default:
         return state;
