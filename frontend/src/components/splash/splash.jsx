f5a16f11354236b13842e1bfc48a3d75a681e2c3
Nick <nbarrameda89@gmail.com>
2022-01-18 21:01:17 -0500

    splash complete

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Navbar from '../nav/navbar_container';
 import '../../assets/css/splash.scss';
 import splashOne from '../../assets/images/splashroad.jpg';
 import splashTwo from '../../assets/images/splashballoonsmall.jpg';
 import splashThree from '../../assets/images/splashplanningsmall.jpg';
+import splashFinal from '../../assets/images/contourmap.jpg'
 
 class Splash extends React.Component{
     render(){
@@ -23,27 +25,27 @@ class Splash extends React.Component{
                 </div>
                 <div className='splash-second'>
                     <div>
+                        <img alt="travel" src={splashTwo} className='splash-two-image' />
+                    </div>
+                    <div className='s2'>
                         <h2>Find new attractions on your way to your destination.</h2>
                         <h3>Draw out a general path and we'll guide you the rest of the way there.</h3>
                     </div>
-                    <div>
-                        <img alt="travel" src={splashTwo} className='splash-two-image' />
-                    </div>
                 </div>
                 <div className='splash-third'>
-                    <div>
-                        <img alt="travel" src={splashThree} className='splash-three-image' />
-                    </div>
-                    <div>
+                    <div className='s3'>
                         <h2>Never miss out on hidden gems along the way.</h2>
                         <h3>Make your roadtrip into a lifetime memory.</h3>
+                    </div>
+                    <div>
+                        <img alt="travel" src={splashThree} className='splash-three-image' />
                     </div>
                 </div>
                 <div className='splash-final'>
                     <div className='splash-final-content'>
-                        <div className='splash-final-bar'><div className='splash-final-inner'></div></div>
                         <h2>Ready to start your journey?</h2>
-                        <button className='to-top'>Join Now</button>
+                        <div><Link to={'/signup'}>Signup</Link></div>
+                        <img alt="travel" src={splashFinal} className='splash-final-image' />
                     </div>
                 </div>
            </div>
