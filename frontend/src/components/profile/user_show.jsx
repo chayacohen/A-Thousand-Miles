01bb5cf4a7ba818b2067fb2e280c8711a9087305
Nick <nbarrameda89@gmail.com>
2022-01-20 15:20:10 -0500

    usershow fixed

--- frontend/src/components/profile/user_show.jsx
+++ frontend/src/components/profile/user_show.jsx
@@ -7,13 +7,13 @@ class UserShow extends React.Component{
         this.state = {
             username: this.props.currentUser.username,
             address: this.props.currentUser.address,
-            main: true
+            load: true,
+            main: true,
         }
         this.userShowToggle = this.userShowToggle.bind(this);
         this.userSettings = this.userSettings.bind(this);
         this.changeUsername = this.changeUsername.bind(this);
         this.changeAddress = this.changeAddress.bind(this);
-        debugger
         }
 
     userShowToggle(){
@@ -27,69 +27,58 @@ class UserShow extends React.Component{
         this.setState({address: e.target.value})
     }
 
-    // componentWillReceiveProps(nextProps) {
-        
-    //     let user = {
-    //         email: this.state.email,
-    //         password: this.state.password,
-    //     };
-
-    //     if (nextProps.signedIn === true) {
-    //         this.props.login(user)
-    //     }
-
-    //     this.setState({ errors: nextProps.errors })
-    // }
+    componentDidMount(){
+        this.props.getUser(this.props.currentUser.id)
+            .then(() => {this.setState({
+                load: false,
+                username: this.props.user[0].username,
+                address: this.props.user[0].address
+            })})
+    }
 
     updateUser() {
-        debugger
         this.props.editUser(this.props.currentUser.id, {username: this.state.username, address: this.state.address})
         
         this.userShowToggle();
     }
 
     userSettings() {
-        debugger
-
-        const {username, email, address} = this.props.currentUser
-        const initials = username.split().map(word => word[0]).join().toUpperCase()
-
-        if (this.state.main === true) {
-            return (
-                <div className='user-main'>
-                <ul className='user-container'>
-                    <li><img onClick={() => this.userShowToggle()} src="https://cdn-icons-png.flaticon.com/512/900/900834.png" alt="" /></li>
-                    <li className='user-bubble'><h1>{initials}</h1></li>
-                    <li>Hi, {username}</li>
-                    <li>{email}</li>
-                    <li>{address}</li>
-                </ul>
-            </div>
-            );
-        } else {
-            return (
-                <div user-edit>
-                <ul className='user-container'>
-                    <li><img onClick={() => this.userShowToggle()} src="https://cdn-icons-png.flaticon.com/512/900/900834.png" alt="" /></li>
-                    <li className='user-bubble'><h1>{initials}</h1></li>
-                    <li>Redefine yourself</li>
-                    <li><input type="text" value={this.state.username} onChange={this.changeUsername}/></li>
-                    <li><input type="text" value={this.state.address} onChange={this.changeAddress}/></li>
-                    <li><button onClick={() => this.updateUser()}>Save Changes</button></li>
-                </ul>
-            </div>
-            );
-        }
+       
     }
         
     render(){
-
-        return(
-            <div className='links-container'>
-                {this.userSettings()}
-            </div>
-        )
-    }
+        if (this.state.load) return (<h1> Loading </h1>);
+        
+            const {username, email, address} = this.props.user[0]
+            const initials = username.split().map(word => word[0]).join().toUpperCase()
+            
+            if (this.state.main === true) {
+                return (
+                    <div className='user-main'>
+                    <ul className='user-container'>
+                        <li><img onClick={() => this.userShowToggle()} src="https://cdn-icons-png.flaticon.com/512/900/900834.png" alt="" /></li>
+                        <li className='user-bubble'><h1>{initials}</h1></li>
+                        <li>Hi, {username}</li>
+                        <li>{email}</li>
+                        <li>{address}</li>
+                    </ul>
+                </div>
+                );
+            } else {
+                return (
+                    <div user-edit>
+                    <ul className='user-container'>
+                        <li><img onClick={() => this.userShowToggle()} src="https://cdn-icons-png.flaticon.com/512/900/900834.png" alt="" /></li>
+                        <li className='user-bubble'><h1>{initials}</h1></li>
+                        <li>Redefine yourself</li>
+                        <li><input type="text" value={this.state.username} onChange={this.changeUsername}/></li>
+                        <li><input type="text" value={this.state.address} onChange={this.changeAddress}/></li>
+                        <li><button onClick={() => this.updateUser()}>Save Changes</button></li>
+                    </ul>
+                </div>
+                );
+            }
+        }    
 };
 
 export default UserShow;
--- frontend/src/components/profile/user_show_container.js
+++ frontend/src/components/profile/user_show_container.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
 import { editUser, getUser } from '../../actions/user_actions';
-import { login } from '../../actions/session_actions';
 import UserShow from './user_show';
 
 const mSTP = (state) => {
-    // debugger
   return {
     currentUser: state.session.user,
     user: Object.values(state.entities.user),
@@ -16,7 +14,6 @@ const mDTP = dispatch => {
   return {
     editUser: (id, data) => dispatch(editUser(id, data)),
     getUser: id => dispatch(getUser(id)),
-    login: user => dispatch(login(user))
   };
 };
 
