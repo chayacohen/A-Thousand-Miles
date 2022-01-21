0ad925226c884e213b09e3065a9ffdc5875e885d
Brian <brianlin1590@gmail.com>
2022-01-21 04:11:53 -0500

    add animation

--- frontend/src/components/itineraryindex/itineraryItem.jsx
+++ frontend/src/components/itineraryindex/itineraryItem.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom";
 import deleteIcon from '../../assets/images/delete.png';
 import pencilIcon from '../../assets/images/pencil.png'
-import map from '../../assets/images/map-temp.png'
+import mapImg from '../../assets/images/map-temp.png'
 const ItineraryItem = ({ itinerary, deleteItinerary, editItinerary, history }) => {
     // debugger;
 
@@ -15,7 +15,7 @@ const ItineraryItem = ({ itinerary, deleteItinerary, editItinerary, history }) =
                 <div className="itinerary-item">
                     <Link to={`/profile/${itinerary._id}`} className="link-container">
                         <div className="map-temp">
-                            <img alt="map" src={map} />
+                            <img alt="map" src={mapImg} />
                         </div>
                     </Link>
                     <div className="card-container">
